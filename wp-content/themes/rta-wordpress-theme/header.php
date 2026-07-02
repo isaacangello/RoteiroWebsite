bde9b98f03174194c9c53cebfff14b91baa53da0
isaacangello <isaacangello@inf.ufpell.edu.br>
2026-07-02 20:57:14 -0300

    feat: favicon multi-tamanhos (16/32/48/96/180/192/512 + apple/android/windows)

--- wp-content/themes/rta-wordpress-theme/header.php
+++ wp-content/themes/rta-wordpress-theme/header.php
@@ -9,9 +9,21 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="icon" href="<?php echo esc_url( get_template_directory_uri() . '/favicon.ico' ); ?>" sizes="32x32">
+	<link rel="icon" href="<?php echo esc_url( get_template_directory_uri() . '/favicon.ico' ); ?>" sizes="48x48">
+	<link rel="icon" href="<?php echo esc_url( get_template_directory_uri() . '/favicon-16x16.png' ); ?>" sizes="16x16" type="image/png">
+	<link rel="icon" href="<?php echo esc_url( get_template_directory_uri() . '/favicon-32x32.png' ); ?>" sizes="32x32" type="image/png">
+	<link rel="icon" href="<?php echo esc_url( get_template_directory_uri() . '/favicon-96x96.png' ); ?>" sizes="96x96" type="image/png">
 	<link rel="icon" href="<?php echo esc_url( get_template_directory_uri() . '/favicon-512.png' ); ?>" sizes="512x512" type="image/png">
-	<link rel="apple-touch-icon" href="<?php echo esc_url( get_template_directory_uri() . '/favicon-512.png' ); ?>">
+	<link rel="apple-touch-icon" href="<?php echo esc_url( get_template_directory_uri() . '/apple-touch-icon-180x180.png' ); ?>">
+	<link rel="apple-touch-icon" sizes="60x60" href="<?php echo esc_url( get_template_directory_uri() . '/apple-touch-icon-60x60.png' ); ?>">
+	<link rel="apple-touch-icon" sizes="76x76" href="<?php echo esc_url( get_template_directory_uri() . '/apple-touch-icon-76x76.png' ); ?>">
+	<link rel="apple-touch-icon" sizes="120x120" href="<?php echo esc_url( get_template_directory_uri() . '/apple-touch-icon-120x120.png' ); ?>">
+	<link rel="apple-touch-icon" sizes="152x152" href="<?php echo esc_url( get_template_directory_uri() . '/apple-touch-icon-152x152.png' ); ?>">
+	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo esc_url( get_template_directory_uri() . '/apple-touch-icon-180x180.png' ); ?>">
+	<link rel="manifest" href="<?php echo esc_url( get_template_directory_uri() . '/site.webmanifest' ); ?>">
+	<meta name="msapplication-TileColor" content="#c40000">
+	<meta name="msapplication-TileImage" content="<?php echo esc_url( get_template_directory_uri() . '/mstile-150x150.png' ); ?>">
+	<meta name="theme-color" content="#c40000">
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class( 'rta-site' ); ?>>
